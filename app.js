b4b1f22a3e5613be33c2fec2326688317e16c568
Ross Edwards <rossedwa@au1.ibm.com>
2017-11-18 23:23:42 +1100

    removed template files

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ var shoppingList = [
   "Milk", "Donuts", "Cookies", "Chocolate", "Peanut Butter", "Pepto Bismol", "Pepto Bismol (Chocolate flavor)", "Pepto Bismol (Cookie flavor)"
 ];
 
-angular.module('ShoppingListApp', [])
-.controller('ShoppingListController', ShoppingListController);
+angular.module('IndexApp', [])
+.controller('IndexController', IndexController);
 
-ShoppingListController.$inject = ['$scope'];
-function ShoppingListController($scope) {
+IndexController.$inject = ['$scope'];
+function IndexController($scope) {
   $scope.shoppingList = shoppingList;
   $scope.authenticate = function() {
-    window.location.replace("https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=22CL4Q&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2FLecture18%2FLandingPage.html&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800");
+    window.location.replace("https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=22CL4Q&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2FLandingPage.html&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800");
   };
 }
 
